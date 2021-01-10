8b07fd1bb4cc0640ee6f2e3ef5c4663bd04e0fb9
Aanchalt14 <aanchalt71@gmail.com>
2021-01-11 01:08:35 +0530

    Add : Rolling dice and get number between 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,13 +2,23 @@
 public class SnakeLadder {
       static int numberofplayer=1;
       static int position;
+      static int diceoutcome=(int)Math.random()*(6-1+1)+1;
+
 
       static void start() {
         int position =0;
         System.out.println("Initial position of player is " +position);
       }
-     public static void main (String args []) {
+
+
+     //UC-2 get number between 1-6
+      static void rolling_dice() {
+      System.out.println( "Dice output :" +diceoutcome);
+     }
+
+
+    public static void main (String args []) {
         start();
-  
+        rolling_dice();
     }
 }
