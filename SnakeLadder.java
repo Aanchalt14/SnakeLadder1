3e321ef141e069d431000f44cafbcb9f6be4ffcb
Aanchalt14 <aanchalt71@gmail.com>
2021-01-11 00:59:34 +0530

    Add : Snake ladder game

---
+++ SnakeLadder.java
@@ -0,0 +1,14 @@
+//UC-1 Initializing position
+public class SnakeLadder {
+      static int numberofplayer=1;
+      static int position;
+
+      static void start() {
+        int position =0;
+        System.out.println("Initial position of player is " +position);
+      }
+     public static void main (String args []) {
+        start();
+  
+    }
+}
